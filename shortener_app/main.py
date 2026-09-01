02264b287a77ed7f78aac763970bafd38ca2db23
fnaderi01 <ffnaderi8@gmail.com>
2026-09-01 17:48:53 -0400

    Add a post endpoint which receives a target URL from users.

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,9 +1,21 @@
 # shortener_app/main.py
 
-from fastapi import FastAPI
+import validators
+from fastapi import FastAPI, HTTPException
+
+from . import schemas
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return "Welcome to the URL Shortener API!"
+
+def raise_bad_request(message) :
+    raise HTTPException(status_code=400, detail=message)
+
+@app.post("/url")
+def create_url(url : schemas.URLBase):
+    if not validators.url(url.target_url) :
+        raise_bad_request("The URL you provided is not valid.")
+    return f"TODO: create database entry for {url.target_url}"
