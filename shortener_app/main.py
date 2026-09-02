67357f3a03591dd3d18b0b653292644228db7d04
fnaderi01 <ffnaderi8@gmail.com>
2026-09-02 14:18:29 -0400

    Setup database to persist the create requests

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,21 +1,48 @@
 # shortener_app/main.py
 
-import validators
-from fastapi import FastAPI, HTTPException
+import secrets
 
-from . import schemas
+import validators
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+from .database import SessionLocal, engine
 
 app = FastAPI()
+""" 
+    Binds your database engine with models.Base.metadata.create_all(). 
+    If the database that you defined in engine doesn’t exist yet, 
+    then it’ll be created with all modeled tables once you run your 
+    app the first time. 
+"""
+models.Base.metadata.create_all(bind=engine)
 
-@app.get("/")
-def read_root():
-    return "Welcome to the URL Shortener API!"
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 def raise_bad_request(message) :
     raise HTTPException(status_code=400, detail=message)
 
-@app.post("/url")
-def create_url(url : schemas.URLBase):
-    if not validators.url(url.target_url) :
-        raise_bad_request("The URL you provided is not valid.")
-    return f"TODO: create database entry for {url.target_url}"
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your provided URL is not valid")
+
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    key = "".join(secrets.choice(chars) for _ in range(5))
+    secret_key = "".join(secrets.choice(chars) for _ in range(8))
+    db_url = models.URL(
+        target_url=url.target_url, key=key, secret_key=secret_key
+    )
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    db_url.url = key
+    db_url.admin_url = secret_key
+
+    return db_url
