e57a51efdec06bd64261a5307c1c28f0d50cb73d
fnaderi01 <ffnaderi8@gmail.com>
2026-09-02 16:49:30 -0400

    Tidy up with accordance to new functions

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,7 +1,5 @@
 # shortener_app/main.py
 
-import secrets
-
 import validators
 from fastapi import Depends, FastAPI, HTTPException, Request
 from fastapi.responses import RedirectResponse
@@ -9,6 +7,7 @@ from sqlalchemy.orm import Session
 
 from . import models, schemas
 from .database import SessionLocal, engine
+from . import crud
 
 app = FastAPI()
 """ 
@@ -39,13 +38,7 @@ def forward_to_target_url(
     request: Request,
     db: Session = Depends(get_db)
 ):
-    db_url = (
-        db.query(models.URL)
-        .filter(models.URL.key == url_key, models.URL.is_active == True)
-        .first()
-    )
-    
-    if db_url:
+    if db_url := crud.get_db_url_by_key(db, url_key):
         return RedirectResponse(db_url.target_url)
     else:
         raise_not_found(request)
@@ -55,16 +48,8 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided URL is not valid")
 
-    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    key = "".join(secrets.choice(chars) for _ in range(5))
-    secret_key = "".join(secrets.choice(chars) for _ in range(8))
-    db_url = models.URL(
-        target_url=url.target_url, key=key, secret_key=secret_key
-    )
-    db.add(db_url)
-    db.commit()
-    db.refresh(db_url)
-    db_url.url = key
-    db_url.admin_url = secret_key
+    db_url = crud.create_db_url(db=db, url=url)
+    db_url.url = db_url.key
+    db_url.admin_url = db_url.secret_key
 
     return db_url
